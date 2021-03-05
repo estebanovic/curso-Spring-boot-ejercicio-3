86b863feedc606e958925936e70740724a3a60c1
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-03-05 00:43:50 -0300

    message

--- src/main/java/com/example/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/example/springboot/form/app/controllers/FormController.java
@@ -7,20 +7,28 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
         Usuario usuario = new Usuario();
+        usuario.setNombre("John");
+        usuario.setApellido("difool");
+        usuario.setIdentificador("123.456.789-K");
+        usuario.setUsername("jdef");
+        usuario.setEmail("jdef@mail.com");
         model.addAttribute("titulo", "Crear usuario");
         model.addAttribute("usuario", usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
         
         model.addAttribute("titulo", "Resultado form");
         
@@ -28,6 +36,7 @@ public class FormController {
             return "form";
         }
         model.addAttribute("usuario", usuario);
+        status.setComplete();
         return "resultado";
     }
 }
--- src/main/java/com/example/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/example/springboot/form/app/models/domain/Usuario.java
@@ -1,30 +1,39 @@
 package com.example.springboot.form.app.models.domain;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Usuario {
 
-    @NotEmpty
+    @NotEmpty(message = "El nombre no puede estar vacio")
     private String nombre;
-    
+
     @NotEmpty
     private String apellido;
-    
+
+    @Pattern(regexp = "[\\d]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+    private String identificador;
+
     @NotEmpty
+    @Size(min = 3, max = 10)
     private String username;
-    
+
     @NotEmpty
     private String password;
-    
+
     @NotEmpty
+    @Email(message = "El formato no es valido")
     private String email;
 
     public Usuario() {
     }
 
-    public Usuario(String nombre, String apellido, String username, String password, String email) {
+    public Usuario(String nombre, String apellido, String identificador, String username, String password, String email) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.identificador = identificador;
         this.username = username;
         this.password = password;
         this.email = email;
@@ -46,7 +55,14 @@ public class Usuario {
         this.apellido = apellido;
     }
 
-    
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
     public String getUsername() {
         return username;
     }
