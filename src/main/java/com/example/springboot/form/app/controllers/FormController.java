7749b4d0715800d59c1a484ea18c8b882f9df72f
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-03-04 23:32:36 -0300

    1

---
+++ src/main/java/com/example/springboot/form/app/controllers/FormController.java
@@ -0,0 +1,33 @@
+package com.example.springboot.form.app.controllers;
+
+import com.example.springboot.form.app.models.domain.Usuario;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("titulo", "Crear usuario");
+        model.addAttribute("usuario", usuario);
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+        
+        model.addAttribute("titulo", "Resultado form");
+        
+        if(result.hasErrors()){
+            return "form";
+        }
+        model.addAttribute("usuario", usuario);
+        return "resultado";
+    }
+}
